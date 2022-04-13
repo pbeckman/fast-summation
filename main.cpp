596d17feaf72af64f6cd9ea3c63dfdd04273d85e
Paul Beckman <pgb8409@crackle1.cims.nyu.edu>
2022-04-13 12:28:47 -0400

    near-field implemented

--- main.cpp
+++ main.cpp
@@ -5,18 +5,20 @@
 #include <iostream>
 
 struct Node {
-  double* x = NULL;    // array of points
-  double* q = NULL;    // array of charges
-  int n = 0;          // number of points 
-  double c = 0;        // center
-  double* w = NULL;    // weights
-  int p = 0;           // number of weights
+  double* x = NULL; // array of points
+  double* q = NULL; // array of charges
+  int*    I = NULL; // array of indices
+  int     n = 0;    // number of points 
+  double  c = 0;    // center of cell
+  double* w = NULL; // weights
+  int     p = 0;    // number of weights
   struct Node* left  = NULL; // left child node
   struct Node* right = NULL; // right child node
  
-  Node(double* _x, double* _q, int _n, double _c, int _p) {
+  Node(double* _x, double* _q, int* _I, int _n, double _c, int _p) {
     x = _x;
     q = _q;
+    I = _I;
     n = _n;
     c = _c;
     p = _p;
@@ -25,11 +27,10 @@ struct Node {
 };
 
 Node* build_tree(
-  double* x, double* q, int n, int max_pts,
-  double a, double b, 
-  int p
+  double* x, double* q, int* I, int n, 
+  int max_pts, double a, double b, int p
   ) {
-  struct Node* tree = new Node(x, q, n, (a+b)/2, p);
+  struct Node* tree = new Node(x, q, I, n, (a+b)/2, p);
 
   // if there are more than max_pts points in the cell, keep subdividing
   if (n > max_pts) {
@@ -44,10 +45,12 @@ Node* build_tree(
     int nright = n - nleft;
     if (nleft > 0 && nright > 0) { // avoid subdividing empty cells
       tree->left  = build_tree(
-        tree->x, tree->q, nleft, max_pts, a, (a+b)/2, p
+        tree->x, tree->q, tree->I, nleft, 
+        max_pts, a, (a+b)/2, p
         );
       tree->right = build_tree(
-        tree->x + nleft, tree->q + nleft, nright, max_pts, (a+b)/2, b, p
+        tree->x + nleft, tree->q + nleft, tree->I + nleft, nright, 
+        max_pts, (a+b)/2, b, p
         );
       
     }
@@ -56,10 +59,10 @@ Node* build_tree(
 }
 
 void compute_weights(Node* tree) {
-  for (int j = 0; j < tree->n; j++) {
+  for (int i = 0; i < tree->n; i++) {
     for (int m = 0; m < tree->p; m++) {
-      // add weight q_j a_m(x[j] - c) = q_j (c - x[j])^m
-      tree->w[m] += tree->q[j] * pow((tree->c - tree->x[j]), m);
+      // add weight q_i a_m(x[i] - c) = q_i (c - x[i])^m
+      tree->w[m] += tree->q[i] * std::pow((tree->c - tree->x[i]), m);
     }
   }
 
@@ -70,8 +73,25 @@ void compute_weights(Node* tree) {
   }
 }
 
-void compute_potential(double* u, int n, Node* tree) {
-  
+void compute_potential(double* u, Node* tree) {
+  // TODO: compute far-field terms
+
+  // traverse the tree
+  if (tree->left) {
+    // continue computing far-field terms by expansion
+    compute_potential(u, tree->left); 
+    compute_potential(u, tree->right);
+  } else {
+    // evaluate near-field directly
+    for (int i = 0; i < tree->n; i++) {
+      for (int j = 0; j < tree->n; j++) {
+        if (i != j) {
+          // add exact potential q_i / |x[i] - x[j]|
+          u[tree->I[i]] += tree->q[i] / std::abs(tree->x[i] - tree->x[j]);
+        }
+      }
+    }
+  }
 }
 
 void barnes_hut(double* u, int n, Node* tree, int p) {
@@ -83,55 +103,42 @@ void barnes_hut(double* u, int n, Node* tree, int p) {
   compute_weights(tree);
 
   // use the precomputed weights to compute the potential
-  compute_potential(u, n, tree);
-
-  // // Evaluate the potential (top to bottom)
-  // for (int l = 0; l < j; l++) {
-  //   for (int k = 0; k < pow(2, l); k++) {
-  //     // write relevant targets into temporary targets vector, where nt_lk gives the number of targets in T_{l,k}
-  //     int nt_lk = get_targets(targets, tree, l, k); 
-  //     for (int i = 0; i < nt_lk; i++) {
-  //       double y = targets[i];
-  //       for (int m = 0; m < p; m++) { // far-field
-  //         // write relevant sources into temporary sources vector, where ns_lk gives the number of sources in the interaction list of T_{l,k}
-  //         int ns_lk = get_interaction_list(sources, tree, l, k); 
-  //         for (j = 0; j < ns_lk; i++) {
-  //           double s = sources[j];
-  //           // TODO: u(i) = u(i) + w(l,k,m) * Sm(xstar(l,s) - y);
-  //         }
-  //       }
-  //       if (l == j) { // near-field
-  //         // TODO: u(i) = u(i) + near-field(y(i), T(J,:));
-  //       }
-  //     }
-  //   }
-  // }
-
+  compute_potential(u, tree);
 }
 
 int main() {
   int n = 100; // number of source / target points
-  int max_pts = 10; // maximum number of points per neighborhood
+  int max_pts = 100; // maximum number of points per neighborhood
   int p = 5; // number of terms in multipole expansion
 
   // draw and sort a set of n uniform random points in [0,1]
   double* x = (double*) malloc(n * sizeof(double));
-  for (int i = 0; i < n; i++) x[i] = ((double)rand()/RAND_MAX);
+  for (int i = 0; i < n; i++) x[i] = ((double)rand()/RAND_MAX); // ((double)i)/n;
   std::sort(x, x+n);
 
   // draw a set of n uniform random charges in [-1, 1]
   double* q = (double*) malloc(n * sizeof(double));
   for (int i = 0; i < n; i++) q[i] = 2*((double)rand()/RAND_MAX) - 1;
 
-  // build a binary tree subdiving our points
-  Node* tree = build_tree(x, q, n, max_pts, 0, 1, p);
+  // make a simple index vector [0,...,n]
+  int* I = (int*) malloc(n * sizeof(int));
+  for (int i = 0; i < n; i++) I[i] = i;
 
-  // allocate the potential
+  // build a binary tree subdiving our points
+  Node* tree = build_tree(x, q, I, n, max_pts, 0, 1, p);
+
+  // allocate the potential and initialize to zero
   double* u = (double*) malloc(n * sizeof(double));
+  for (int i = 0; i < n; i++) u[i] = 0;
 
   // run barnes_hut
   barnes_hut(u, n, tree, p);
 
-  // // display charges at source points
-  // for (int i = 0; i < n; i++) printf("u(%.3f) = %.3f\n", x[i], q[i]);
+  // display potential at source / target points
+  for (int i = 0; i < n; i++) printf("u(%.3f) = %.3f\n", x[i], u[i]);
+
+  free(x);
+  free(q);
+  free(I);
+  free(u);
 }
