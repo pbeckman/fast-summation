7c4653f2dab667fdbd22ea812fd73826ca1f207f
Paul Beckman <pgb8409@crackle1.cims.nyu.edu>
2022-04-14 15:12:05 -0400

    far-field addition implemented

--- main.cpp
+++ main.cpp
@@ -12,25 +12,31 @@ struct Node {
   double  c = 0;    // center of cell
   double* w = NULL; // weights
   int     p = 0;    // number of weights
-  struct Node* left  = NULL; // left child node
-  struct Node* right = NULL; // right child node
+  struct Node* parent = NULL; // parent node
+  struct Node* left   = NULL; // left child node
+  struct Node* right  = NULL; // right child node
  
-  Node(double* _x, double* _q, int* _I, int _n, double _c, int _p) {
+  Node(
+    double* _x, double* _q, int* _I, 
+    int _n, double _c, int _p, Node* _parent
+    ) {
     x = _x;
     q = _q;
     I = _I;
     n = _n;
     c = _c;
     p = _p;
+    parent = _parent;
     w = (double*) malloc(p * sizeof(double));
   }
 };
 
 Node* build_tree(
   double* x, double* q, int* I, int n, 
-  int max_pts, double a, double b, int p
+  int max_pts, double a, double b, int p,
+  Node* parent
   ) {
-  struct Node* tree = new Node(x, q, I, n, (a+b)/2, p);
+  struct Node* tree = new Node(x, q, I, n, (a+b)/2, p, parent);
 
   // if there are more than max_pts points in the cell, keep subdividing
   if (n > max_pts) {
@@ -46,11 +52,11 @@ Node* build_tree(
     if (nleft > 0 && nright > 0) { // avoid subdividing empty cells
       tree->left  = build_tree(
         tree->x, tree->q, tree->I, nleft, 
-        max_pts, a, (a+b)/2, p
+        max_pts, a, (a+b)/2, p, tree
         );
       tree->right = build_tree(
         tree->x + nleft, tree->q + nleft, tree->I + nleft, nright, 
-        max_pts, (a+b)/2, b, p
+        max_pts, (a+b)/2, b, p, tree
         );
       
     }
@@ -61,7 +67,8 @@ Node* build_tree(
 void compute_weights(Node* tree) {
   for (int i = 0; i < tree->n; i++) {
     for (int m = 0; m < tree->p; m++) {
-      // add weight q_i a_m(x[i] - c) = q_i (c - x[i])^m
+      // add weight term q_i a_m(x[i] - c) 
+      // where a_m(x[i] - c) = (c - x[i])^m
       tree->w[m] += tree->q[i] * std::pow((tree->c - tree->x[i]), m);
     }
   }
@@ -73,8 +80,82 @@ void compute_weights(Node* tree) {
   }
 }
 
+Node* get_left_interaction(Node* tree) {
+  Node* node = tree->parent; // initialize node pointer we'll search tree with
+  int l = 1; // how many levels have we moved up in our search
+  
+  // ascend the tree until we are not the left child of our parent
+  while (node->parent->left == node) { 
+    node = node->parent;
+    l++;
+  }
+
+  // now we're the right child of our parent
+  // so we cross the tree to be in the left branch
+  node = node->parent->left;
+
+  // descend the tree to the right until we are one level above the base node
+  for (; l > 1; l--) node = node->right;
+
+  // if the base node was originally a left node, the right child here
+  // is in the near field, so give only the left child
+  if (tree->parent->left == tree) {
+    node = node->left;
+  }
+
+  return node;
+}
+
+Node* get_right_interaction(Node* tree) {
+  Node* node = tree->parent; // initialize node pointer we'll search tree with
+  int l = 1; // how many levels have we moved up in our search
+  
+  // ascend the tree until we are not the right child of our parent
+  while (node->parent->right == node) { 
+    node = node->parent;
+    l++;
+  }
+
+  // now we're the left child of our parent
+  // so we cross the tree to be in the right branch
+  node = node->parent->right;
+
+  // descend the tree to the left until we are one level above the base node
+  for (; l > 1; l--) node = node->left;
+
+  // if the base node was originally a right node, the left child here
+  // is in the near field, so give only the right child
+  if (tree->parent->right == tree) {
+    node = node->right;
+  }
+
+  return node;
+}
+
+void add_far_field(double* u, Node* tree, Node* node) {
+  // evaluate far-field using multipole expansions
+  for (int i = 0; i < tree->n; i++) {
+    for (int m = 0; m < tree->p; m++) {
+      // add approximate potential term w_{i,m} * S_m(c - x[i]) 
+      // where S_m(c - x[i]) = 1 / (|c - x[i]|*(c - x[i])^m)
+      u[tree->I[i]] += node->w[m] * (
+        std::abs(node->c - node->x[i])*std::pow(node->c - node->x[i], m)
+        );
+    }
+  }
+}
+
 void compute_potential(double* u, Node* tree) {
-  // TODO: compute far-field terms
+  // the top two levels have empty interaction lists
+  if (tree->parent && tree->parent->parent) { 
+    Node* node = NULL;
+    
+    node = get_left_interaction(tree);
+    add_far_field(u, tree, node);
+
+    node = get_right_interaction(tree);
+    add_far_field(u, tree, node);
+  }
 
   // traverse the tree
   if (tree->left) {
@@ -108,7 +189,7 @@ void barnes_hut(double* u, int n, Node* tree, int p) {
 
 int main() {
   int n = 100; // number of source / target points
-  int max_pts = 100; // maximum number of points per neighborhood
+  int max_pts = 10; // maximum number of points per neighborhood
   int p = 5; // number of terms in multipole expansion
 
   // draw and sort a set of n uniform random points in [0,1]
@@ -125,7 +206,7 @@ int main() {
   for (int i = 0; i < n; i++) I[i] = i;
 
   // build a binary tree subdiving our points
-  Node* tree = build_tree(x, q, I, n, max_pts, 0, 1, p);
+  Node* tree = build_tree(x, q, I, n, max_pts, 0, 1, p, NULL);
 
   // allocate the potential and initialize to zero
   double* u = (double*) malloc(n * sizeof(double));
